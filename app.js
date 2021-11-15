b487b0064498abbdb715628b0062fad6bd43fda4
fiosman <fares.osman@outlook.com>
2021-11-14 22:25:32 -0500

    connect to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = process.env.MONGOURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
