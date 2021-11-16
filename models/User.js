a941da91799cddfedf70ba14d1f6f128132707fd
fiosman <fares.osman@outlook.com>
2021-11-16 02:23:29 -0500

    Update user model

--- models/User.js
+++ models/User.js
@@ -18,27 +18,6 @@ const UserSchema = new Schema(
   { timestamps: true }
 );
 
-UserSchema.pre("save", function (next) {
-  const user = this;
-
-  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-    if (err) return next(err);
-
-    bcrypt.hash(user.passwordDigest, salt, function (err, hash) {
-      if (err) return next(err);
-      user.passwordDigest = hash;
-      next();
-    });
-  });
-});
-
-UserSchema.methods.comparePassword = function (candidatePassword, passwordMatched) {
-  bcrypt.compare(candidatePassword, this.passwordDigest, function (err, isMatch) {
-    if (err) return passwordMatched(err, null);
-    passwordMatched(null, isMatch);
-  });
-};
-
 const User = mongoose.model("user", UserSchema);
 
 module.exports = User;
